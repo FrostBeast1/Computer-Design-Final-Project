6f6abe0b0021d698f97080c5ddb36d74b7eb594d
bradyk-017 <sloth-ayes.0t@icloud.com>
2026-05-10 17:25:14 -0500

    Added Microsequencer Unit
    Added Microsequencer.v (Top-level), as well as Micro_MUX.v and Micro_Memory.v. Updated original Quartus project with new files and simulation waveform file. Altered excel sheet to now contain both a 4-bit instruction and 5-bit instruction page.

---
+++ Microseqencer.v
@@ -0,0 +1,54 @@
+// Microsequencer Control Unit
+// Note that opcode "hardwiring" for address translation is handled inside
+// the Micro_MUX.v file.
+
+module Microseqencer #(
+    parameter OP_WIDTH = 3,
+    parameter ADDR_WIDTH = 4,
+    parameter MICRO_WIDTH = 15
+) (
+    clk_i, op_i, micro_o, debug_o
+);
+
+input wire clk_i;
+input wire [OP_WIDTH-1:0] op_i;
+output [MICRO_WIDTH-1:0] micro_o;
+output [ADDR_WIDTH-1+1:0] debug_o;
+
+// Local Address register
+reg [ADDR_WIDTH-1:0] next_addr;
+
+// Interconnecting wires
+wire [ADDR_WIDTH-1:0] MUX_to_reg;
+
+// Wires to split Micro_Memory output
+wire select;
+wire [MICRO_WIDTH-2:0] micro_op;
+wire [ADDR_WIDTH-1:0] mem_to_MUX;
+
+assign debug_o = {select, MUX_to_reg};
+
+// Sub-module Instantiations
+Micro_MUX #(
+    .OP_WIDTH(OP_WIDTH),
+    .ADDR_WIDTH(ADDR_WIDTH)
+) micro_mux (
+    .sel_i(select),
+    .op_i(op_i),
+    .addr_i(mem_to_MUX),
+    .addr_o(MUX_to_reg)
+);
+
+// Memory output updates on negedge
+Micro_Memory micro_mem (
+    .clock(clk_i),
+    .address(next_addr),
+    .q({select, micro_o, mem_to_MUX})
+);
+
+// Next address register updates on positive edge
+always @(posedge clk_i) begin
+    next_addr <= MUX_to_reg;
+end
+
+endmodule
